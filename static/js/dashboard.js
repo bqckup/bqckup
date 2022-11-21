0af6ae4a62c1b816a6392ae2c8e859ffb003cae5
thisnugroho <this.nugroho@gmail.com>
2022-11-21 17:01:15 +0800

    update

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,16 +1,6 @@
 function dashboard() {
   return {
     bqckups: false,
-    async fetchBqckups() {
-      let request = await fetch("/backup/list");
-      let response = await request.json();
-      if (request.status >= 500) {
-        return alert("Failed to fetch backup, please check logs");
-      }
-      this.bqckups = response;
-    },
-    async init() {
-      this.fetchBqckups();
-    },
+    init() {},
   };
 }
