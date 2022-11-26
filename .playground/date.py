d64096a9f9a7d39433f4fc45557dea0eb539a55e
thisnugroho <this.nugroho@gmail.com>
2022-11-26 14:50:08 +0800

    update playground

--- .playground/date.py
+++ .playground/date.py
@@ -1,4 +1,7 @@
-from datetime import date
 from datetime import datetime
+def checkIfMidnight():
+    now = datetime.now()
+    seconds_since_midnight = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+    return seconds_since_midnight == 0
 
-print(datetime.now().strftime("%H:%M:%S"))
+print(checkIfMidnight())
