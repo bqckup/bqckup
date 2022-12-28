89d8d02bb54f477104b3fd5236f8cd1f752528b0
thisnugroho <this.nugroho@gmail.com>
2022-12-28 14:33:54 +0800

    remove playground

--- .playground/date.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-def checkIfMidnight():
-    now = datetime.now()
-    seconds_since_midnight = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
-    return seconds_since_midnight == 0
-
-print(checkIfMidnight())
--- .playground/to_yml.py
+++
@@ -1,6 +0,0 @@
-import yaml
-
-x = {"name": "Dede Nugroho", "options": {"Gender": "Boys"}}
-
-xx = yaml.dump(x, default_flow_style=False)
-print(xx)
