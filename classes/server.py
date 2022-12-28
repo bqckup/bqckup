05fa1086ec472e10479dac41853d1b037e143f7d
thisnugroho <this.nugroho@gmail.com>
2022-12-28 09:57:00 +0800

    able to get public ip

--- classes/server.py
+++ classes/server.py
@@ -1,8 +1,14 @@
-import shutil
+import shutil, requests
 class Server:
     def __init__(self):
         pass
     
     def get_storage_information(self):
         return shutil.disk_usage('/')
-        
+    
+    def ip(self):
+        try:
+            return requests.get('https://ifconfig.me').text
+        except Exception:
+            print("Failed to get IP address, using socket instead")
+            return "0.0.0.0"
