734d989f62c8c509e8eff87ffaa02f87f2588f0c
thisnugroho <this.nugroho@gmail.com>
2023-02-07 16:22:02 +0800

    add: subclass to manage yml

---
+++ classes/has_yml.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+from classes.file import File
+from classes.yml_parser import Yml_Parser
+import os
+
+class HasYML(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get_config_path(self):
+        pass
+
+    def save_config(self, name: str, data: dict):
+        import ruamel.yaml as yaml
+        with open(os.path.join(self.get_config_path(), f"{name}.yml"), "w+") as stream:
+            yaml = yaml.YAML()
+            yaml.indent(sequence=4, offset=2)
+            yaml.dump(data, stream)
+
+    def get_list_file_config(self):
+        return [f for f in File().get_file_list(self.get_config_path()) if f.endswith('.yml')]
+
+    def _parse_config(self, name: str):
+        return Yml_Parser.parse(os.path.join(self.get_config_path(), f"{name}.yml"))
+    
+    def _parse_all_config(self):
+        results = []
+        for config in self.get_list_file_config():
+            parsed_config = Yml_Parser.parse(config)
+            results.append(parsed_config)
+            parsed_config['name'] = os.path.basename(config).replace('.yml', '')
+
+        return list(results)
