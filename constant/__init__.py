533ed48f6ec67c253b6974ea0978078d66e4c9eb
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2024-08-11 23:13:52 +0800

    Update version

--- constant/__init__.py
+++ constant/__init__.py
@@ -13,7 +13,7 @@ SITE_CONFIG_PATH = path.join(BQ_PATH, 'sites')
 CONFIG_PATH = path.join(BQ_PATH, 'bqckup.cnf')
 
 # Bqckup Information
-VERSION = "1.2.7"
+VERSION = "1.2.8"
 
 # YOURLS Credentials
 YOURLS_HOST = ""
