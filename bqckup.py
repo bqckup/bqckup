2fe0b49aca0810fc120a3275a39c16e5d0db595f
Reja Revaldy F <rejarevaldy30@gmail.com>
2024-08-16 08:14:28 +0800

    Improvment Download Latest: str.path => os.getcwsd()

--- bqckup.py
+++ bqckup.py
@@ -339,10 +339,10 @@ def download_latest(name: str, target: str = None, silent: bool = False):
                 if not confirm_with_timeout(prompt_message, timeout=10):
                     target = typer.prompt(typer.style("Please enter the target directory path", fg=typer.colors.YELLOW))
                 else:
-                    target =  str(Path().absolute())
+                    target =  os.getcwd()
             target = validate_path(target)
         else:
-            target =  str(Path().absolute()) if not target else validate_path(target)
+            target =  os.getcwd() if not target else validate_path(target)
 
         if target.is_dir():
             print(f"[green]\nTarget directory: {target}\n[/green]")
