75bc597e223ecfbb80ed052869e1f0086bb34c43
ahmad ridho ramadhan <ahmadridhor0@gmail.com>
2024-08-19 16:38:33 +0800

    change full path to path base on path site

--- classes/tar.py
+++ classes/tar.py
@@ -7,11 +7,10 @@ class Tar:
     
     def compress(self, source: Union[str, list, dict], output: str, exclude_paths = []) -> str:
 
-        def exclude_path(tarinfo, path):
-            # Check if the path should be excluded
-            file_path = os.path.join(path, tarinfo.name)
+        def exclude_path(tarinfo, folder_name):
+            file_name = tarinfo.name.replace(folder_name, "")
             for exclude_path in exclude_paths:
-                if file_path.startswith(exclude_path):
+                if file_name.startswith(exclude_path):
                     print(f"Excluding {tarinfo.name}")
                     return None
             return tarinfo
@@ -20,13 +19,12 @@ class Tar:
             if type(source) != str:
                 for path in source:
                     # remove last folder to get base path
-                    folder_name = path.split("/")[-1]
-                    base_path = path.replace(folder_name, "")
+                    folder_name = path.split("/")[-1] + "/"
 
                     if not os.path.exists(path):
                         print(f"Skipped, {path} not found")
                         continue
-                    tar.add(path, arcname=os.path.basename(path), filter=lambda tarinfo: exclude_path(tarinfo, base_path))
+                    tar.add(path, arcname=os.path.basename(path), filter=lambda tarinfo: exclude_path(tarinfo, folder_name))
             else:
                 tar.add(path, arcname=os.path.basename(path))
             tar.close()
