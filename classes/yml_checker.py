0f3c2e995d746bda92f29d985af0ff0f0659e1a1
ahmad ridho ramadhan <ahmadridhor0@gmail.com>
2024-08-20 09:29:04 +0800

    add error style in yml checker

--- classes/yml_checker.py
+++ classes/yml_checker.py
@@ -1,6 +1,7 @@
 from constant import BQ_PATH, STORAGE_CONFIG_PATH, SITE_CONFIG_PATH
 from classes.file import File
 from classes.yml_parser import Yml_Parser
+from rich import print
 
 class Yml_Checker:
     def checker():
@@ -8,15 +9,16 @@ class Yml_Checker:
 
         # check if storage is exist
         if not File().is_exists(STORAGE_CONFIG_PATH):
-            print ("\nStorage config not found \n")
+            print ("\n [red]Storage config not found[/red]")
             is_error = True
 
         # storage check
         try:
             Yml_Parser.parse(STORAGE_CONFIG_PATH)
         except Exception as e:
-            print ("\nError in storage config \n")
+            print ("\n [red]Error in storage config[/red] \n")
             is_error = True
+        
 
         # site check
         files = File().get_file_list(SITE_CONFIG_PATH)
@@ -26,7 +28,7 @@ class Yml_Checker:
             try:
                 Yml_Parser.parse(file)
             except Exception as e:
-                print(f"error in site config on {file} \n")
+                print(f" [red]Error in site config on[/red] {file} \n")
                 is_error = True
         
         if is_error:
