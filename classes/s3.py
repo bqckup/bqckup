d961d463fb1734bdf6932b91b36b578b74da53ec
Aspian <muhammad.aspian.d@gmail.com>
2025-12-24 14:23:27 +0800

    refactor: reduce max attempt

--- classes/s3.py
+++ classes/s3.py
@@ -26,7 +26,7 @@ class s3(object):
     _instances = {}
     config = Config(
         retries={
-            "max_attempts": 10,
+            "max_attempts": 5,
             "mode": "adaptive",
         }
     )
