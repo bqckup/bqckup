017962d5be55a64d89411425efdfec1d31ad7838
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2025-12-26 13:30:05 +0800

    Update __init__.py

--- constant/__init__.py
+++ constant/__init__.py
@@ -15,7 +15,7 @@ CONFIG_PATH = path.join(BQ_PATH, 'bqckup.cnf')
 RUSTIC_CONFIG_PATH = "/etc/rustic"
 
 # Bqckup Information
-VERSION = "1.8.1"
+VERSION = "1.8.2"
 
 # YOURLS Credentials
 YOURLS_HOST = ""
