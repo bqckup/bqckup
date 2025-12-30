ab631d3382cafbd30f42ec46c47fedec3cb11985
Aspian <muhammad.aspian.d@gmail.com>
2025-12-30 08:31:09 +0800

    update classes/bqckup.py

--- classes/bqckup.py
+++ classes/bqckup.py
@@ -507,9 +507,9 @@ class Bqckup:
                         "2. There might be an issue with the database backup process.\n\n"
                         "We recommend the following steps:\n"
                         "1. Check the storage (S3) bucket {bucket_name}. If the database size is less than 1 KB or seems unusual, it likely means the backup did not complete successfully.\n"
-                        "2. Attempt to force a backup by running `bqckup --site {domain_name} --force` to ensure the backup process is functioning correctly."
+                        "2. Attempt to force a backup by running `bqckup --site {domain_name} --force` to ensure the backup process is functioning correctly.\n"
+                        "If this was a mistake, please create issue here: https://github.com/bqckup/bqckup"
                     ),
-                    "footer": "If this was a mistake, please create issue here: https://github.com/bqckup/bqckup",
                     "additional_data": {
                         "name": "File name",
                         "value": os.path.basename(compressed_file),
